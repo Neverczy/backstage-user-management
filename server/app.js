76941811ac48aaf22dd4e757f9980a1f2ebf2039
nevermore <never_czy@163.com>
2022-06-13 23:25:15 +0800

    2022/6/13

--- server/app.js
+++ server/app.js
@@ -8,6 +8,7 @@ const userSign = require('./router/user_sign')
 const app = express()
 
 app.use(cors())
+app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use((req, res, next) => {
   res.cc = function (err, status = 400) {
@@ -26,6 +27,7 @@ app.use(jwt({secret: config.jwtSecretKey, algorithms: ["HS256"]})
 app.use('/api', userSign)
 
 app.use((err, req, res, next) => {
+  console.log(err)
   if (err.name = 'UnauthorizedError') return res.cc('身份认证失败')
 })
 
--- server/router-handler/user_sign.js
+++ server/router-handler/user_sign.js
@@ -15,7 +15,13 @@ exports.signUp = (req, res) => {
     if (result.length > 0) return res.cc('用户名已被占用')
     userSignInfo.password = bcrypt.hashSync(userSignInfo.password, 8)
     sql = `insert into vm_users set ?`
-    db.query(sql, {username: userSignInfo.username, password: userSignInfo.password}, (err, result) => {
+    db.query(sql, {
+      username: userSignInfo.username,
+      password: userSignInfo.password,
+      nickname: userSignInfo.nickname,
+      email: userSignInfo.email,
+      is_admin: userSignInfo.username === 'admin' ? 0 : 1
+    }, (err, result) => {
       if (err) return res.cc(err)
       if (result.affectedRows !== 1) return res.cc('注册失败,请稍后再试')
       res.cc('注册成功', 200)
@@ -25,6 +31,67 @@ exports.signUp = (req, res) => {
 
 exports.login = (req, res) => {
   const userLoginInfo = req.body
+  const menu = [
+    [
+      {
+        path: '/',
+        name: 'home',
+        label: '首页',
+        icon: 's-home',
+        url: 'home'
+      },
+      {
+        path: '/mall',
+        name: 'mall',
+        label: '商品管理',
+        icon: 'video-play',
+        url: 'mall'
+      },
+      {
+        path: '/user',
+        name: 'user',
+        label: '用户管理',
+        icon: 'user',
+        url: 'user'
+      },
+      {
+        label: '其他',
+        icon: 'location',
+        children: [
+          {
+            path: '/page1',
+            name: 'page1',
+            label: '页面1',
+            icon: 'setting',
+            url: 'others/pageOne'
+          },
+          {
+            path: '/page2',
+            name: 'page2',
+            label: '页面2',
+            icon: 'setting',
+            url: 'others/pageTwo'
+          }
+        ]
+      }
+    ],
+    [
+      {
+        path: '/',
+        name: 'home',
+        label: '首页',
+        icon: 's-home',
+        url: 'home'
+      },
+      {
+        path: '/video',
+        name: 'video',
+        label: '商品管理',
+        icon: 'video-play',
+        url: 'mall'
+      }
+    ]
+  ]
   if (!userLoginInfo.username.trim() || !userLoginInfo.password.trim())
     return res.cc('用户名或密码不能为空')
   userLoginInfo.password = userLoginInfo.password.trim()
@@ -39,7 +106,9 @@ exports.login = (req, res) => {
     res.send({
       status: 200,
       msg: '登录成功',
+      data: menu[result[0]['is_admin']],
       token: 'Bearer ' + tokenStr
     })
+    console.log(`用户"${userLoginInfo.username}"登陆了`)
   })
 }
